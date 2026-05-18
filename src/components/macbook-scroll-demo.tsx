1a14707470912f173f2f2abd0f2f0d02a6e4d5d2
Rohan Modi <rohanmodi1014@gmail.com>
2026-05-18 18:23:50 +0000

    Initial commit

---
+++ src/components/macbook-scroll-demo.tsx
@@ -0,0 +1,37 @@
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+
+export function MacbookScrollDemo() {
+  return (
+    <div className="-mt-32 w-full overflow-hidden bg-zinc-900 dark:bg-[#0B0B0F]">
+      <MacbookScroll
+        title={
+  <span className="text-4xl font-bold text-white dark:text-white">
+    All your freelancing. <br /> All in one place.
+  </span>
+}
+        badge={
+          <a href="https://peerlist.io/manuarora">
+            <Badge className="h-10 w-10 -rotate-12 transform" />
+          </a>
+        }
+        src="/linear.png"
+        showGradient={false}
+      />
+    </div>
+  );
+}
+
+const Badge = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 56 56"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      {/* keep all your existing <path> tags here */}
+    </svg>
+  );
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        dmsans: ['"DM Sans"', "sans-serif"],
+      },
+    },
+  },
+};
